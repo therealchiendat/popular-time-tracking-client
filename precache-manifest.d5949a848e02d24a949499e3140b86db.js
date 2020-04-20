7018b4f6651300d8fbcdce3cb91e8c24713adb1a
Chien Dat Nguyen Dinh <chiendat@Mac-Dat.local>
2020-04-20 12:07:08 -0300

    Updates

---
+++ precache-manifest.d5949a848e02d24a949499e3140b86db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a54f9dd633ad2a378ed341502ad4e94",
+    "url": "/popular-time-tracking-client/index.html"
+  },
+  {
+    "revision": "d677ba3c45f23b41fb90",
+    "url": "/popular-time-tracking-client/static/css/main.55cd18aa.chunk.css"
+  },
+  {
+    "revision": "0fe44a296ddbda1e5041",
+    "url": "/popular-time-tracking-client/static/js/2.9823d6d9.chunk.js"
+  },
+  {
+    "revision": "d677ba3c45f23b41fb90",
+    "url": "/popular-time-tracking-client/static/js/main.3cadd0dc.chunk.js"
+  },
+  {
+    "revision": "5d860a9d23cb1b371057",
+    "url": "/popular-time-tracking-client/static/js/runtime~main.199feead.js"
+  }
+]);
